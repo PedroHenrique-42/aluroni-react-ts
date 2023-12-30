4d045cda1fdd2b9e3d4d7aaf10cb1944e277c6f4
Pedro Henrique <phlferezin03@gmail.com>
2023-12-30 20:50:02 -0300

    feat: organizando o projeto inicial.

---
+++ src/pages/menu/index.tsx
@@ -0,0 +1,11 @@
+import style from './menu.module.scss';
+
+export default function Menu() {
+    return (
+        <main>
+            <nav className='menu'>
+                
+            </nav>
+        </main>
+    )
+}
