8560be3706386bb84cf5269f2a711e82e9880335
Pedro Henrique <phlferezin03@gmail.com>
2023-12-31 16:32:32 -0300

    feat: adicionando logo no componente menu, adicionando reset css e adicionando base url de imports como a pasta src.

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,10 +1,11 @@
 import style from './menu.module.scss';
+import { ReactComponent as Logo } from 'assets/logo.svg';
 
 export default function Menu() {
     return (
         <main>
-            <nav className='menu'>
-                
+            <nav className={style.menu}>
+                <Logo />
             </nav>
         </main>
     )
