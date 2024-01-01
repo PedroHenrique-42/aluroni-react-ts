2ad37e6a3f118f4fafe0d52459fdff435a81cf21
Pedro Henrique <phlferezin03@gmail.com>
2023-12-31 21:19:21 -0300

    feat: adicionando header e input de texto.

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,12 +1,28 @@
-import style from './menu.module.scss';
+import styles from './menu.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
+import Search from './search';
+import { useState } from 'react';
 
 export default function Menu() {
+    const [search, setSearch] = useState("");
+
     return (
         <main>
-            <nav className={style.menu}>
+            <nav className={styles.navbar}>
                 <Logo />
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A casa do código e da massa
+                </div>
+            </header>
+            <section className={styles.menu}>
+                <h3 className={styles.menu__title}>Cardápio</h3>
+                <Search
+                    search={search}
+                    setSearch={setSearch}
+                />
+            </section>
         </main>
     )
 }
---
+++ src/pages/menu/search/index.tsx
@@ -0,0 +1,21 @@
+import styles from "./search.module.scss";
+import { CgSearch } from "react-icons/cg";
+
+interface Props {
+    search: string;
+    setSearch: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Search({ search, setSearch }: Props) {
+    return <div className={styles.search}>
+        <input
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+        />
+        <CgSearch
+            size={20}
+            color="#4C4D5E"
+        />
+    </div>
+
+}
